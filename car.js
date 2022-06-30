bc25273f81a947af8f563e502f74633e96b2bfdf
Nishijama <nathanplociennik@gmail.com>
2022-06-30 15:03:58 +0200

    Implemented road and ray-casting

--- car.js
+++ car.js
@@ -11,14 +11,16 @@ class Car {
         this.speed = 0;
         this.acceleration = 0.2;
         this.maxSpeed = 3;
-        this.friction = 0.05;
+        this.friction = 0.01;
         this.angle = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
-    update(){
+    update(roadBorders){
         this.#move();   
+        this.sensor.update(roadBorders);
     }
 
     #move() {
@@ -80,6 +82,8 @@ class Car {
 
         ctx.fill();
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 
 }
